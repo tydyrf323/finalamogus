ef2f6965589b21704a24e857312383d57e3d3b98
thydyrfy <elfernando1a@gmail.com>
2022-10-22 23:32:21 -0400

    Sql commands actualizado

--- sql/commands.sql
+++ sql/commands.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 18-10-2022 a las 07:10:19
+-- Tiempo de generación: 23-10-2022 a las 05:31:25
 -- Versión del servidor: 10.4.21-MariaDB
 -- Versión de PHP: 8.0.11
 
@@ -36,7 +36,6 @@ CREATE TABLE `compras` (
   `Cantidad` int(11) NOT NULL,
   `MontoTotal` decimal(9,2) NOT NULL,
   `PrecioVenta` decimal(9,2) NOT NULL,
-  `Localizacion` tinyint(4) NOT NULL DEFAULT 0,
   `Observacion` varchar(50) DEFAULT NULL,
   `FechaCompra` timestamp NOT NULL DEFAULT current_timestamp(),
   `FechaVencimiento` timestamp NULL DEFAULT NULL
@@ -46,34 +45,57 @@ CREATE TABLE `compras` (
 -- Volcado de datos para la tabla `compras`
 --
 
-INSERT INTO `compras` (`IdCompra`, `Codigo`, `IdUsuario`, `IdProveedor`, `Descripcion`, `Cantidad`, `MontoTotal`, `PrecioVenta`, `Localizacion`, `Observacion`, `FechaCompra`, `FechaVencimiento`) VALUES
-(11, 'S-2', 'ferch', 'BSL', '1', 1, '1.00', '1.00', 0, '1', '2022-09-11 04:00:00', NULL),
-(12, 'S-3', 'ferch', 'BSL', '1', 1, '1.00', '1.00', 0, '1', '2022-09-11 04:00:00', NULL),
-(13, 'M-1', 'ferch', 'BSL', '1', 1, '1.00', '1.00', 1, '1', '2022-09-11 04:00:00', NULL),
-(32, 'S-4', 'ferch', 'NOSE', 'Silla (plastico)', 1, '12.00', '12.00', 0, NULL, '2022-09-19 04:00:00', NULL),
-(33, 'S-5', 'ferch', 'BSL', 'Silla (plastico)', 4, '12.00', '3.00', 1, NULL, '2022-09-20 04:00:00', NULL),
-(34, 'S-6', 'ferch', 'BSL', 'Silla (plastico)', 4, '12.00', '3.00', 0, NULL, '2022-09-20 04:00:00', NULL),
-(35, 'S-7', 'ferch', 'BSL', 'Silla (plastico)', 4, '12.00', '3.00', 0, NULL, '2022-09-20 04:00:00', NULL),
-(36, 'M-2', 'ferch', 'BSL', 'Mesa Madera', 4, '12.00', '3.00', 1, NULL, '2022-09-20 04:00:00', NULL),
-(37, 'S-8', 'ferch', 'BSL', '1', 2, '1.00', '0.50', 0, '1', '2022-09-26 04:00:00', NULL),
-(58, 'S-10', 'ferch', 'BSL', '1', 100, '100.00', '149.43', 0, '1', '2022-09-27 04:00:00', NULL),
-(60, 'S-11', 'ferch', 'BSL', '1', 1, '1.00', '1.00', 1, '1', '2022-09-27 04:00:00', NULL),
-(62, 'S-12', 'ferch', 'BSL', '1', 1, '1.00', '1.00', 0, '1', '2022-09-27 04:00:00', NULL),
-(146, 'S-13', 'ferch', 'BSL', 'Mesa Madera', 1, '1.00', '0.01', 1, NULL, '2022-09-27 04:00:00', NULL),
-(147, 'S-14', 'ferch', 'BSL', 'Mesa Madera', 1, '1.00', '0.01', 0, NULL, '2022-09-27 04:00:00', NULL),
-(148, 'S-15', 'ferch', 'BSL', 'Mesa Madera', 1, '1.00', '0.01', 0, NULL, '2022-09-27 04:00:00', NULL),
-(149, 'S-16', 'ferch', 'BSL', 'Mesa Madera', 1, '1.00', '0.01', 0, NULL, '2022-09-27 04:00:00', NULL),
-(150, 'S-17', 'ferch', 'BSL', 'Mesa Madera', 1, '1.00', '0.01', 0, NULL, '2022-09-27 04:00:00', NULL),
-(151, 'S-18', 'ferch', 'BSL', 'Mesa Madera', 1, '1.00', '0.01', 0, NULL, '2022-09-27 04:00:00', NULL),
-(152, 'S-19', 'ferch', 'BSL', 'Mesa Madera', 1, '1.00', '0.01', 0, NULL, '2022-09-27 04:00:00', NULL),
-(153, 'S-20', 'ferch', 'BSL', 'Mesa Madera', 1, '1.00', '0.01', 0, NULL, '2022-09-27 04:00:00', NULL),
-(165, 'S1-1', 'ferch', 'BSL', 'Cómoda Azul', 3, '60.00', '30.00', 0, NULL, '2022-10-09 04:00:00', NULL),
-(166, 'S1-2', 'ferch', 'BSL', 'Cómoda Azul', 1, '60.00', '90.00', 0, NULL, '2022-10-09 04:00:00', NULL),
-(167, 'R-1', 'ferch', 'BSL', 'Ropero', 1, '70.00', '105.00', 0, NULL, '2022-10-09 04:00:00', NULL),
-(168, 'R-2', 'ferch', 'BSL', 'Ropero', 1, '70.00', '105.00', 0, NULL, '2022-10-09 04:00:00', NULL),
-(169, 'R-3', 'ferch', 'BSL', 'Ropero', 1, '70.00', '105.00', 0, NULL, '2022-10-09 04:00:00', NULL),
-(170, 'R-4', 'ferch', 'BSL', 'Ropero', 1, '70.00', '105.00', 0, NULL, '2022-10-09 04:00:00', NULL),
-(171, 'R-5', 'ferch', 'BSL', 'Ropero', 1, '70.00', '105.00', 0, NULL, '2022-10-09 04:00:00', NULL);
+INSERT INTO `compras` (`IdCompra`, `Codigo`, `IdUsuario`, `IdProveedor`, `Descripcion`, `Cantidad`, `MontoTotal`, `PrecioVenta`, `Observacion`, `FechaCompra`, `FechaVencimiento`) VALUES
+(11, 'S-2', 'ferch', 'BSL', '1', 0, '1.00', '1.00', '1', '2022-09-11 04:00:00', NULL),
+(12, 'S-3', 'ferch', 'BSL', '1', 1, '1.00', '1.00', '1', '2022-09-11 04:00:00', NULL),
+(13, 'M-1', 'ferch', 'BSL', '1', 1, '1.00', '1.00', '1', '2022-09-11 04:00:00', NULL),
+(32, 'S-4', 'ferch', 'NOSE', 'Silla (plastico)', 1, '12.00', '12.00', NULL, '2022-09-19 04:00:00', NULL),
+(33, 'S-5', 'ferch', 'BSL', 'Silla (plastico)', 4, '12.00', '3.00', NULL, '2022-09-20 04:00:00', NULL),
+(34, 'S-6', 'ferch', 'BSL', 'Silla (plastico)', 4, '12.00', '3.00', NULL, '2022-09-20 04:00:00', NULL),
+(35, 'S-7', 'ferch', 'BSL', 'Silla (plastico)', 4, '12.00', '3.00', NULL, '2022-09-20 04:00:00', NULL),
+(36, 'M-2', 'ferch', 'BSL', 'Mesa Madera', 4, '12.00', '3.00', NULL, '2022-09-20 04:00:00', NULL),
+(37, 'S-8', 'ferch', 'BSL', '1', 2, '1.00', '0.50', '1', '2022-09-26 04:00:00', NULL),
+(58, 'S-10', 'ferch', 'BSL', '1', 35, '100.00', '149.43', '1', '2022-09-27 04:00:00', NULL),
+(60, 'S-11', 'ferch', 'BSL', '1', 1, '1.00', '1.00', '1', '2022-09-27 04:00:00', NULL),
+(62, 'S-12', 'ferch', 'BSL', '1', 1, '1.00', '1.00', '1', '2022-09-27 04:00:00', NULL),
+(146, 'S-13', 'ferch', 'BSL', 'Mesa Madera', 1, '1.00', '0.01', NULL, '2022-09-27 04:00:00', NULL),
+(147, 'S-14', 'ferch', 'BSL', 'Mesa Madera', 1, '1.00', '0.01', NULL, '2022-09-27 04:00:00', NULL),
+(148, 'S-15', 'ferch', 'BSL', 'Mesa Madera', 1, '1.00', '0.01', NULL, '2022-09-27 04:00:00', NULL),
+(149, 'S-16', 'ferch', 'BSL', 'Mesa Madera', 1, '1.00', '0.01', NULL, '2022-09-27 04:00:00', NULL),
+(150, 'S-17', 'ferch', 'BSL', 'Mesa Madera', 1, '1.00', '0.01', NULL, '2022-09-27 04:00:00', NULL),
+(151, 'S-18', 'ferch', 'BSL', 'Mesa Madera', 1, '1.00', '0.01', NULL, '2022-09-27 04:00:00', NULL),
+(152, 'S-19', 'ferch', 'BSL', 'Mesa Madera', 1, '1.00', '0.01', NULL, '2022-09-27 04:00:00', NULL),
+(153, 'S-20', 'ferch', 'BSL', 'Mesa Madera', 1, '1.00', '0.01', NULL, '2022-09-27 04:00:00', NULL),
+(165, 'S1-1', 'ferch', 'BSL', 'Cómoda Azul', 3, '60.00', '30.00', NULL, '2022-10-09 04:00:00', NULL),
+(166, 'S1-2', 'ferch', 'BSL', 'Cómoda Azul', 1, '60.00', '90.00', NULL, '2022-10-09 04:00:00', NULL),
+(167, 'R-1', 'ferch', 'BSL', 'Ropero', 1, '70.00', '105.00', NULL, '2022-10-09 04:00:00', NULL),
+(168, 'R-2', 'ferch', 'BSL', 'Ropero', 1, '70.00', '105.00', NULL, '2022-10-09 04:00:00', NULL),
+(169, 'R-3', 'ferch', 'BSL', 'Ropero', 1, '70.00', '105.00', NULL, '2022-10-09 04:00:00', NULL),
+(170, 'R-4', 'ferch', 'BSL', 'Ropero', 1, '70.00', '105.00', NULL, '2022-10-09 04:00:00', NULL),
+(171, 'R-5', 'ferch', 'BSL', 'Ropero', 1, '70.00', '105.00', NULL, '2022-10-09 04:00:00', NULL),
+(173, 'C1-1', 'ferch', 'BSL', 'Catres', 0, '100.00', '3.00', NULL, '2022-10-22 04:00:00', NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `misc`
+--
+
+CREATE TABLE `misc` (
+  `telf` varchar(30) NOT NULL,
+  `calle` varchar(50) NOT NULL,
+  `nombre` varchar(50) NOT NULL,
+  `nit` int(10) NOT NULL,
+  `actividad` varchar(30) NOT NULL,
+  `porcentaje` int(3) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `misc`
+--
+
+INSERT INTO `misc` (`telf`, `calle`, `nombre`, `nit`, `actividad`, `porcentaje`) VALUES
+('60503503', 'Av. Vanguardia', 'Fernando', 0, 'Polleria', 30);
 
 -- --------------------------------------------------------
 
@@ -97,7 +119,8 @@ INSERT INTO `productos` (`id`, `producto`, `codigo`, `fecha`) VALUES
 (3, 'Mesa Madera', 'M', '2022-09-12 01:09:43'),
 (4, 'AAaa', 'A', '2022-09-15 04:37:10'),
 (5, 'Cómoda Azul', 'S1', '2022-10-10 00:57:20'),
-(6, 'Ropero', 'R', '2022-10-10 01:05:58');
+(6, 'Ropero', 'R', '2022-10-10 01:05:58'),
+(7, 'Catres', 'C1', '2022-10-22 18:15:23');
 
 -- --------------------------------------------------------
 
@@ -133,10 +156,19 @@ CREATE TABLE `tienda` (
   `IdProveedor` varchar(30) NOT NULL,
   `Descripcion` varchar(50) NOT NULL,
   `qty` int(11) NOT NULL,
-  `PrecioDeVenta` int(11) NOT NULL,
+  `PrecioDeVenta` decimal(9,2) NOT NULL,
   `Fecha` timestamp NOT NULL DEFAULT current_timestamp(),
   `Observacion` varchar(50) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `tienda`
+--
+
+INSERT INTO `tienda` (`IdTIenda`, `Codigo`, `IdProveedor`, `Descripcion`, `qty`, `PrecioDeVenta`, `Fecha`, `Observacion`) VALUES
+(5, 'S-2', 'BSL', '1', 0, '1.00', '2022-10-22 18:21:15', '1'),
+(7, 'S-10', 'BSL', '1', 7, '149.43', '2022-10-22 18:52:36', '1'),
+(9, 'C1-1', 'BSL', 'Catres', 48, '3.00', '2022-10-22 22:41:47', '');
 
 -- --------------------------------------------------------
 
@@ -156,7 +188,8 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`user`, `password`, `role`, `fechaCreacion`) VALUES
-('ferch', '123', 0, '2022-09-01 02:42:08');
+('ferch', '123', 0, '2022-09-01 02:42:08'),
+('USER', '123', 1, '2022-10-22 19:29:02');
 
 -- --------------------------------------------------------
 
@@ -193,7 +226,20 @@ INSERT INTO `ventas` (`IdVenta`, `Codigo`, `IdUsuario`, `IdProveedor`, `Descripc
 (24, 'M-1', 'ferch', 'BSL', '2', 2, 'X11', '12.00', '1.00', '2022-10-08 04:00:00', 'EL'),
 (25, 'M-1', 'ferch', 'BSL', '3', 2, 'X11', '12.00', '1.00', '2022-10-08 04:00:00', 'EL'),
 (27, 'S-3', 'ferch', 'BSL', '1', 1, 'X11', '21.00', '1.00', '2022-10-09 04:00:00', 'FERCH'),
-(34, 'R-2', 'ferch', 'BSL', 'Ropero', 1, 'X11', '189.00', '105.00', '2022-10-09 04:00:00', '');
+(34, 'R-2', 'ferch', 'BSL', 'Ropero', 1, 'X11', '189.00', '105.00', '2022-10-09 04:00:00', ''),
+(44, 'S-2', 'ferch', 'BSL', '1', 1, 'X11', '12.00', '1.00', '2022-10-18 04:00:00', ''),
+(49, 'S-2', 'ferch', 'BSL', '1', 1, 'X11', '2.00', '1.00', '2022-10-22 04:00:00', 'YO'),
+(55, 'S-10', 'ferch', 'BSL', '1', 1, 'X11', '1000.00', '149.43', '2022-10-22 04:00:00', ''),
+(56, 'S-10', 'ferch', 'BSL', '1', 1, 'X11', '150.00', '149.43', '2022-10-22 04:00:00', ''),
+(57, 'S-10', 'ferch', 'BSL', '1', 1, 'X11', '1000.00', '149.43', '2022-10-22 04:00:00', ''),
+(58, 'S-10', 'ferch', 'BSL', '1', 1, 'X11', '1000.00', '149.43', '2022-10-22 04:00:00', ''),
+(59, 'S-10', 'ferch', 'BSL', '1', 1, 'X11', '1000.00', '149.43', '2022-10-22 04:00:00', ''),
+(60, 'S-10', 'ferch', 'BSL', '1', 1, 'X121', '10000.00', '149.43', '2022-10-22 04:00:00', ''),
+(61, 'C1-1', 'ferch', 'BSL', 'Catres', 2, 'X11', '5.00', '3.00', '2022-10-22 04:00:00', 'Luis'),
+(63, 'C1-1', 'ferch', 'NOSE', 'AAAAAAAAAA silaa tv nose aA', 1, 'X11', '100.00', '1.00', '2022-09-30 01:00:55', ''),
+(64, 'C1-1', 'ferch', 'BSL', 'Silla', 12, 'X11', '100.00', '1.00', '2021-10-29 01:01:40', ''),
+(65, 'C1-1', 'ferch', 'BSL', 'Silla', 12, 'X11', '100.00', '1.00', '2022-11-24 01:04:15', ''),
+(66, 'C1-1', 'ferch', 'BSL', 'Silla', 12, 'X11', '100.00', '1.00', '2022-02-01 01:13:42', '');
 
 --
 -- Índices para tablas volcadas
@@ -252,25 +298,25 @@ ALTER TABLE `ventas`
 -- AUTO_INCREMENT de la tabla `compras`
 --
 ALTER TABLE `compras`
-  MODIFY `IdCompra` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=173;
+  MODIFY `IdCompra` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=174;
 
 --
 -- AUTO_INCREMENT de la tabla `productos`
 --
 ALTER TABLE `productos`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT de la tabla `tienda`
 --
 ALTER TABLE `tienda`
-  MODIFY `IdTIenda` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `IdTIenda` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT de la tabla `ventas`
 --
 ALTER TABLE `ventas`
-  MODIFY `IdVenta` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=44;
+  MODIFY `IdVenta` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=67;
 
 --
 -- Restricciones para tablas volcadas
